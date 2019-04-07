7046a1a3cb95a686c31a45c57afe59d79c62f4db
nikasmorozov <solarisocean@gmail.com>
2019-04-07 13:25:28 +0300

    Pirma veikianti sistema

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,33 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
 var browserSync = require('browser-sync').create();
 
-gulp.task('sync', function() {
- browserSync.init({
- proxy: "business.dev",
- files: "*.html,*.css,*.php,css/*css"
+gulp.task('browser-sync', function() {
+  browserSync.init({
+    server: {
+      baseDir: './'
+    },
+  })
+})
+
+gulp.task('sass', function() {
+	return gulp.src('sass/styles.scss')
+		.pipe(sass({
+			outputStyle: 'normal'
+		}))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+		.pipe(gulp.dest(''))
+		.pipe(browserSync.stream());
 });
-});
+
+gulp.task('watch',['browser-sync'], function(){
+  gulp.watch('*.html', browserSync.reload); 
+})
+
+gulp.task('default', ['sass', 'browser-sync', 'watch'], function() {
+	gulp.watch('sass/*.scss', ['sass']);
+})
